8fb084c2baa06f9ec2f9fae971c0302b0f3c2fb1
tamaR-797 <3295797@gmail.com>
2025-12-27 19:57:28 +0200

    add css

--- webApi/obj/Debug/net8.0/.net.AssemblyInfo.cs
+++ webApi/obj/Debug/net8.0/.net.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cf1c65ff5ba9cd280049b8031e3e7f9a267c6b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9b460c36934a13698c241b24259bc61e524706")]
 [assembly: System.Reflection.AssemblyProductAttribute(".net")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
